13496282cd84e8e5db924503a32ceea907d64152
Alon4ic <o.redjko@gmail.com>
2025-08-22 16:32:41 +0300

    Created my journej

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,43 @@
+import Cta from '@/components/CTA';
+import CompanionCard from '@/components/CompanionCard';
+import CompanionsList from '@/components/CompanionsList';
+import { Button } from '@/components/ui/button';
+import { recentSessions } from '@/constants';
+import {
+    getAllCompanions,
+    getRecentSession,
+} from '@/lib/actions/companion.actions';
+import { getSubjectColor } from '@/lib/utils';
+import React from 'react';
 
+const Page = async () => {
+    const companions = await getAllCompanions({ limit: 3 });
+    const recentSessionCompanions = await getRecentSession(10);
 
-import Cta from '@/components/CTA'
-import CompanionCard from '@/components/CompanionCard'
-import CompanionsList from '@/components/CompanionsList'
-import { Button } from '@/components/ui/button'
-import { recentSessions } from '@/constants'
-import React from 'react'
+    return (
+        <main>
+            <h1 className="text-2xl underline">Popular Companions</h1>
 
-const Page = () => {
-  return (
-    <main>
-      <h1 className='text-2xl underline'>Popular Companions</h1>
-      
-      <section className='home-section'>
-        <CompanionCard
-          id='123'
-          name='Neura the Brainy Explorer'
-          topic='Neural Network of the Brain'
-          subject='science'
-          duration={45}
-          color='#ffda6e'
-        />
-        <CompanionCard
-          id='456'
-          name='Countsy the Number Wizard'
-          topic='Derivatives & Integrals'
-          subject='maths'
-          duration={30}
-          color='#e5d0ff'
-        />
-        <CompanionCard
-          id='789'
-          name='Verba the Vocabulary Builder'
-          topic='English Literature'
-          subject='language'
-          duration={30}
-          color='#BDE7FF'
-        />
-      </section>
+            <section className="home-section">
+                {companions.map((companion) => (
+                    <CompanionCard
+                        key={companion.id}
+                        {...companion}
+                        color={getSubjectColor(companion.subject)}
+                    />
+                ))}
+            </section>
 
-      <section className='home-section'>
-        <CompanionsList
-          title='Recently completed sessions'
-          companions={recentSessions}
-          classNames='w-2/3 max-lg:w-full'
-        />
-        <Cta />
-      </section>
-    </main>
-  )
-}
+            <section className="home-section">
+                <CompanionsList
+                    title="Recently completed sessions"
+                    companions={recentSessionCompanions}
+                    classNames="w-2/3 max-lg:w-full"
+                />
+                <Cta />
+            </section>
+        </main>
+    );
+};
 
-export default Page
+export default Page;
